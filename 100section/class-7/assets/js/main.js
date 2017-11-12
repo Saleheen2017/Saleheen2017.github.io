a9edc9fee3ea643ed32894085eb87e346669775c
Saleheen2017 <32485335+Saleheen2017@users.noreply.github.com>
2017-11-12 20:12:26 +0600

    100section/class-7

---
+++ 100section/class-7/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".staff-list").owlCarousel({
+           
+            items: 4,
+            autoplay: false,
+            margin: 50,
+            loop: true,
+            nav: true,
+            navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+        });
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
