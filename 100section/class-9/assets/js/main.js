0ef2c901f994d4ff00a46f9f896bdde608260567
Saleheen2017 <32485335+Saleheen2017@users.noreply.github.com>
2017-11-17 01:13:42 +0600

    100section/class-9

---
+++ 100section/class-9/assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".testimonial-carousel").owlCarousel({
+            
+            items: 3,
+            loop: true,
+            nav: false,
+            dots: true,
+            margin: 30,
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
